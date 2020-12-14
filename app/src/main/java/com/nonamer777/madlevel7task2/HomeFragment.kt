73000e8b26c7ae66f93dec97b873ba985459fb84
Oscar Wellner <o.wellner@telfort.nl>
2020-12-14 09:03:19 +0100

    Refactored boiletplate code

---
+++ app/src/main/java/com/nonamer777/madlevel7task2/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.nonamer777.madlevel7task2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A [Fragment] subclass that serves as the starting screen after the splash screen.
+ */
+class HomeFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/nonamer777/madlevel7task2/QuestionsOverviewFragment.kt
@@ -0,0 +1,24 @@
+package com.nonamer777.madlevel7task2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A [Fragment] subclass where users can view the questions that are part of the quiz.
+ */
+class QuestionsOverviewFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_questions_overview, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
